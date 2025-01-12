90f86b4cd135d8eceb340aa388f819e38558adfc
marshyi <marshyidev@gmail.com>
2025-01-12 15:59:12 +0100

    Fix unescaped characters

--- src/views/contact.tsx
+++ src/views/contact.tsx
@@ -6,9 +6,9 @@ export default function Contact() {
     <Section id="contact" title="Let's get in touch!" tabIndex={9}>
       <p className="text-pretty leading-loose mt-8">
         Got a <span className="font-bold underline decoration-4 decoration-blue-700">project in mind</span>, or just looking to connect?<br />
-        I'd love to hear from you!<br />
+        I&apos;d love to hear from you!<br />
         <br />
-        Whether it's to collaborate or just say hello, feel free to reach out.<br />
+        Whether it&apos;s to collaborate or just say hello, feel free to reach out.<br />
         You can reach me at <Link href="mailto:hi@marshyi.dev" target="_blank"  className="font-mono bg-blue-700 hover:bg-red-600">hi@marshyi.dev</Link>
       </p>
     </Section>
